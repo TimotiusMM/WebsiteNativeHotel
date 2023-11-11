a46813ccf6068e821a1527c34298a3a2d7319c1f
MarceinoModo <timmarmod@gmail.com>
2023-11-11 10:33:57 +0700

    coba

--- login.php
+++ login.php
@@ -52,7 +52,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>LOGIN - HOTELLIN</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <style>
         body {
