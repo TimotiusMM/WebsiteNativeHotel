cc73325c48fee32285c7448b6447cab00aa1632e
Aceelino <timmarmod@gmail.com>
2023-11-11 14:31:04 +0700

    baru login4

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Multi User Level</title>
+    <title>LOGIN - HOTELLIN</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
     <link rel="stylesheet" href="desain.css"> <!-- Link to your external CSS file -->
